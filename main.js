d6ca16e6381c869ace36508fa287c0f669c9a70c
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2024-12-08 17:09:42 +0200

    Add event handler for when numbers are pressed

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+
+
+
 function add(a, b){
   return a + b;
 }
@@ -31,3 +34,53 @@ function operate(a, b, operator){
   }
   
 }
+
+
+
+function addButtonEventListener(){
+
+  let calculator = document.querySelector(".calculator");
+  let display = document.querySelector(".display");
+  let button;
+
+  calculator.addEventListener("click", (event) => {
+    button = event.target;
+    console.log(button.id);
+
+    switch(button.id){
+      case "0":
+        display.textContent += "0";
+        break;
+      case "1":
+        display.textContent += "1";
+        break;
+      case "2":
+        display.textContent += "2";
+        break;
+      case "3":
+        display.textContent += "3";
+        break;
+      case "4":
+        display.textContent += "4";
+        break;
+      case "5":
+        display.textContent += "5";
+        break;
+      case "6":
+        display.textContent += "6";
+        break;
+      case "7":
+        display.textContent += "7";
+        break;
+      case "8":
+        display.textContent += "8";
+        break;
+      case "9":
+        display.textContent += "9";
+        break;
+    }
+  });
+
+}
+
+addButtonEventListener();
